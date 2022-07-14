8ecefb8dded77df94cf2cd6c6c71228f6bb65baf
Franklin Obasi <franklinobasy@gmail.com>
2022-07-14 13:22:25 +0100

    modified main.c

--- operations_set_A.c
+++ operations_set_A.c
@@ -81,6 +81,7 @@ void _pint(stack_t **stack, unsigned int line_number)
 void _pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *top = NULL;
+
 	if (!(*stack) || !stack)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
