5a5214e15631c9079bd342f8925cc3d85e074c6f
Franklin Obasi <franklinobasy@gmail.com>
2022-07-14 03:39:33 +0100

    corrected error output

--- error_handlers.c
+++ error_handlers.c
@@ -29,7 +29,7 @@ void push_failure(FILE *fd, char *line, stack_t *stack, int count)
  */
 void no_command(FILE *fd, char *line, stack_t *stack, char *count, int item)
 {
-	fprintf(stderr, "L%u: unknown instruction %s\n", item, count);
+	fprintf(stderr, "L%u: unknown instruction %s\n", count, item);
 	fclose(fd);
 	free(line);
 	free_stack(stack);
