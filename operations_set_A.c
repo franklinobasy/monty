489c2f2724595a4fbb461dc4103fdf156c031936
Franklin Obasi <franklinobasy@gmail.com>
2022-07-14 12:27:54 +0100

    2. pop

--- operations_set_A.c
+++ operations_set_A.c
@@ -83,7 +83,7 @@ void _pop(stack_t **stack, unsigned int line_number)
 	stack_t *top = NULL;
 	if (!(*stack) || !stack)
 	{
-		fprintf(stderr, "L<%d>: can't pop an empty stack\n", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
